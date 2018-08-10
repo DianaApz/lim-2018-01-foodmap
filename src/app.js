7b6de3fb63b50ca778276cdfe2a5b00319e33689
DianaApz <kari_dia_12@hotmail.com>
2018-08-10 13:20:29 -0500

    act filter of type

--- src/app.js
+++ src/app.js
@@ -1,12 +1,49 @@
 let rest=document.getElementById('rest');
+let part=document.getElementById('part');
 
-var database = firebase.database();
+let database = firebase.database();
 firebase.database().ref().child('restaurantes/San Borja').once('value', function(data) {
-    let ar=Object.values(data.val());
-    console.log(ar);
-    ar.forEach(element => {
-        console.log(element.nombre);
-    });
-
+    let restaurants=Object.values(data.val());
     
+    showFood(restaurants)
 });
+
+const filterTypeFood=(restaurants,search)=>{
+    let searchType = restaurants.filter(restaurant => restaurant.tipo.toLowerCase().indexOf(search.toLowerCase()) > -1);
+    return searchType;
+};
+
+const showFood=(filter)=>{
+    let template='';
+    filter.forEach((restaurant)=>{
+      template+=
+      `<div class="container">
+        <div class="text">
+          <p style= "font-weight:bold;">${restaurant.nombre}</p></br>
+        </div>
+        <div class="text">
+          <ul >
+           <li> Dirección : ${(restaurant.direccion)}</li>
+           <li> Horario : ${(restaurant.horario)}</li>
+           <li> Tipo : ${(restaurant.tipo)}</li>
+          </ul>
+        </div>  
+      </div>`
+    });
+    part.innerHTML=template;
+}
+
+rest.addEventListener('keyup',(event)=>{
+    const valueSearch=event.target.value;
+    
+    firebase.database().ref().child('restaurantes/San Borja').once('value', function(data) {
+      let restaurants=Object.values(data.val());
+      const filter=filterTypeFood(restaurants,valueSearch)
+      showFood(filter)
+
+
+    });
+})
+
+
+
